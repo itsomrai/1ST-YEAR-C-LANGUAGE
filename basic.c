9fc736445fc8b92e5331426e7d4c324436c80120
itsomrai <work.omrai@gmail.com>
2026-09-02 08:46:38 +0530

    UPDATE

--- basic.c
+++ basic.c
@@ -3,8 +3,12 @@ int main()
 {
 int a=10;/*int a=10,b=20*/
 int b=20;
-int c;
-c=a+b;
-printf("The sum of a and b is %d",c);
+printf("The sum of a and b is %d",a+b);
+
+char c='K';
+float d=10.776;
+printf("Value of a = %d",a);
+printf("\nValue of c = %c",c);
+printf("\nValue of d = %.2f",d);
 return 0;
 }
