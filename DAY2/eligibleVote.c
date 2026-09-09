62f16517c77bff6137930661beac13a614a427e5
itsomrai <work.omrai@gmail.com>
2026-09-09 08:23:26 +0530

    BASICS

---
+++ DAY2/eligibleVote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int age;
+printf("Enter the age");
+scanf("%d",&age);
+if(age>=18){
+    printf("Eligible to vote");
+}
+else{
+    printf("Not Eligible");
+}
+return 0;
+}
